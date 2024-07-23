955163bfa42013462bc605862c37b69222ed485c
SpicyIslandFruits <spicyisland1998@gmail.com>
2024-07-24 00:22:10 +0900

    renamed bundemo di part

--- cmd/bundemo/index.ts
+++ cmd/bundemo/index.ts
@@ -2,26 +2,26 @@ import * as elysia from "../../elysia";
 
 // コマンドライン引数からポート番号を取得する関数
 function getPortFromArgs(): number {
-    const args = process.argv.slice(2);
-    let port = 4001; // デフォルト値
+  const args = process.argv.slice(2);
+  let port = 4001; // デフォルト値
 
-    for (let i = 0; i < args.length; i++) {
-        if (args[i] === '--port' && i + 1 < args.length) {
-            const portValue = parseInt(args[i + 1], 10);
-            if (!isNaN(portValue)) {
-                port = portValue;
-                break;
-            }
-        }
+  for (let i = 0; i < args.length; i++) {
+    if (args[i] === "--port" && i + 1 < args.length) {
+      const portValue = parseInt(args[i + 1], 10);
+      if (!isNaN(portValue)) {
+        port = portValue;
+        break;
+      }
     }
+  }
 
-    return port;
+  return port;
 }
 
 // ポート番号を取得
 const port = getPortFromArgs();
 
 // Elysiaを起動
-elysia.launch(port);
+elysia.bundemo(port);
 
 console.log(`Listening on http://localhost:${port} ...`);
--- elysia/bundemo.ts
+++ elysia/bundemo.ts
@@ -1,10 +1,13 @@
 import Elysia from "elysia";
 
-export function launch(port: number) {
+export function bundemo(port: number) {
   // ここで設定する関数をinternalに作る。
   // データの変換があればここでやる、あんまなさそうだけど
   new Elysia()
-    .get("bundemo/", () => "bunで自分用にjupyter notebookをホストするサイト作る予定")
+    .get(
+      "bundemo/",
+      () => "bunで自分用にjupyter notebookをホストするサイト作る予定"
+    )
     .get("bundemo/hi", () => "hi")
     .get("*", () => "404 not found")
     .listen(port);
