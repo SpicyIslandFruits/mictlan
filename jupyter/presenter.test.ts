69703990302573871e383616e716a0b074a86309
SpicyIslandFruits <spicyisland1998@gmail.com>
2024-07-27 17:16:16 +0900

    minor fix package.json

--- jupyter/presenter.test.ts
+++ jupyter/presenter.test.ts
@@ -5,7 +5,7 @@ import * as nbviewer from "../internal/nbviewer";
 
 test("jupyter render", async () => {
   const presenter = new Presenter();
-  const text = await Bun.file("web/notebook/demo.ipynb").text();
+  const text = await Bun.file("web/notebook/leanmemo.ipynb").text();
   const notebook = make<nbviewer.Notebook>();
   const output = await presenter.render(notebook(text));
   console.log("html: ", output);
